a8e9c0aa5e553a603138b76d59f1281b684eac41
Harry <Harryb382003@users.noreply.github.com>
2026-06-17 22:37:15 -0400

    Add metadata promotion candidates

--- lib/QBTL/Config.pm
+++ lib/QBTL/Config.pm
@@ -5,19 +5,34 @@ use common::sense;
 use feature qw( signatures );
 
 use Config::Std {def_sep => '='};
-
+use Cwd            qw(abs_path);
+use File::Basename qw(dirname);
 use File::Spec;
 
 sub new ( $class, %arg ) {
-  my $home = $arg{home} // $ENV{HOME} // die 'HOME is not set';
+
+  my $home         = $arg{home} // $ENV{HOME} // die 'HOME is not set';
+  my $user_config  = File::Spec->catfile( $home, 'QBTL', '.qbtlrc' );
+  my $lib_qbtl_dir = dirname( __FILE__ );
+  my $lib_dir      = dirname( $lib_qbtl_dir );
+  my $repo_root    = abs_path( dirname( $lib_dir ) );
+  my $repo_config  = File::Spec->catfile( $repo_root, '.qbtlrc' );
+  my $config_path  = $user_config;
+
+  if ( !-e $config_path && -e $repo_config ) {
+    $config_path = $repo_config;
+  }
 
   my %self = (
-               home        => $home,
-               config_path => File::Spec->catfile( $home, 'QBTL', '.qbtlrc' ),
-               db_path     => File::Spec->catfile( $home, 'QBTL', 'qbtl.db' ),
-               qbt_url                     => 'http://localhost:8080',
-               time_format                 => 'full',
-               metadata_promoter_threshold => 20, );
+
+              home             => $home,
+              config_path      => $config_path,
+              user_config_path => $user_config,
+              repo_config_path => $repo_config,
+              db_path     => File::Spec->catfile( $home, 'QBTL', 'qbtl.db' ),
+              qbt_url     => 'http://localhost:8080',
+              time_format => 'full',
+              metadata_promoter_threshold => 20, );
 
   my $self = bless \%self, $class;
 
@@ -40,10 +55,12 @@ sub _load_config_file ( $self ) {
   my %config;
   read_config $path => %config;
 
-  if ( defined $config{metadata}{promoter_threshold} ) {
-  $self->{metadata_promoter_threshold}
-    = $config{metadata}{promoter_threshold};
-}
+  if ( exists $config{metadata}
+       && defined $config{metadata}{promoter_threshold} )
+  {
+    $self->{metadata_promoter_threshold} =
+        $config{metadata}{promoter_threshold};
+  }
 
   if ( exists $config{time_format} ) {
     my $format = $config{time_format}{time};
@@ -77,7 +94,16 @@ sub metadata_promoter_threshold ( $self ) {
   return $self->{metadata_promoter_threshold};
 }
 
-sub qbt_url     ( $self ) { return $self->{qbt_url}; }
+sub qbt_url ( $self ) { return $self->{qbt_url}; }
+
+sub _repo_root ( $self ) {
+  my $lib_qbtl_dir = dirname( __FILE__ );         # .../lib/QBTL
+  my $lib_dir      = dirname( $lib_qbtl_dir );    # .../lib
+  my $repo_root    = dirname( $lib_dir );         # repo root
+
+  return abs_path( $repo_root );
+}
+
 sub time_format ( $self ) { return $self->{time_format}; }
 
 1;
